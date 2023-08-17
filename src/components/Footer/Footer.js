cf020349a0a93a0cee112e238ac16906abd25cf3
jSohnData <jihos@me.com>
2023-08-17 01:41:27 -0400

    FIXED mozart's ghost

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,22 +5,24 @@ export default function Footer() {
     const repoUrl = "https://github.com/jsohndata/bocacode-c9";
 
     return (
-      <footer>
-        <Container className="mt-5 mb-5">
-          <Row className="justify-content-center text-center">
-            <Col md={6}>
-              <a href={repoUrl} target="_blank" rel="noreferrer" >
-                  <Github color="#000000" size={27} />
-                  <p><small>See Code in Github</small></p>
-              </a>
+      <>
+        <footer>
+          <Container className="mt-5 mb-5">
+            <Row className="justify-content-center text-center">
+              <Col md={6}>
+                <a href={repoUrl} target="_blank" rel="noreferrer" >
+                    <Github color="#000000" size={27} />
+                    <p><small>See Code in Github</small></p>
+                </a>              
+              </Col>
+            </Row>
+          </Container>
+        </footer>
 
-              <p id="mozarts-ghost"><a href="https://notiempo.lol/mozartsghost/18"
-                rel="noreferrer"
-                alt="Click on it and press control-shift."
-                title="Click on it and press control-shift.">π</a></p>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
+        <p id="mozarts-ghost"><a href="https://notiempo.lol/mozartsghost/17"
+        rel="noreferrer"
+        alt="Click on it and press control-shift."
+        title="Click on it and press control-shift.">π</a></p>
+      </>
     )
 }
